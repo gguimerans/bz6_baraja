72f457fe71af12639a6742cd0b208918555eda90
Gonzalo <gonzaloguimerans@gmail.com>
2020-11-11 00:34:57 +0100

    funcion baraja

--- baraja.py
+++ baraja.py
@@ -1,13 +1,10 @@
 palos = ["o", "c", "e", "b"]
 numeros = ["A", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
 
-baraja = []
 
 def crearBaraja():
+    baraja = []
     for numero in numeros:    
             for palo in palos:
                 baraja.append(numero+palo)
     return baraja
-
-crearBaraja()
-print(baraja)
