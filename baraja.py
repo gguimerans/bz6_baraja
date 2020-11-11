1d3fb441b0c5236ca0fecfaa25510ddcd35e8189
Gonzalo <gonzaloguimerans@gmail.com>
2020-11-11 23:04:57 +0100

    Funcion barajar cartas

--- baraja.py
+++ baraja.py
@@ -1,3 +1,5 @@
+import random
+
 palos = ["o", "c", "e", "b"]
 numeros = ["A", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
 
@@ -8,3 +10,14 @@ def crearBaraja():
             for palo in palos:
                 baraja.append(numero+palo)
     return baraja
+
+
+def barajar(lista_de_naipes):
+    for i in range(len(lista_de_naipes)):
+        nueva_pos = random.randrange(len(lista_de_naipes))
+        aux = lista_de_naipes[nueva_pos]
+        lista_de_naipes[nueva_pos] = lista_de_naipes[i]
+        lista_de_naipes[i] = aux
+    return lista_de_naipes
+
+print (barajar(crearBaraja()))
