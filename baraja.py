0d20a083827ae5718001b870104b867a99ed8bb3
Gonzalo <gonzaloguimerans@gmail.com>
2020-11-12 22:27:08 +0100

    Version final de baraja = crear baraja, barajar, añadir jugadores y rondas

--- baraja.py
+++ baraja.py
@@ -1,23 +1,30 @@
 import random
 
-palos = ["o", "c", "e", "b"]
-numeros = ["A", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
 
 
-def crearBaraja():
+def creaBaraja(palos, numeros):
     baraja = []
-    for numero in numeros:    
-            for palo in palos:
-                baraja.append(numero+palo)
+    for palo in palos:
+        for numero in numeros:
+            baraja.append(numero+palo)
     return baraja
+
+
+def intercambio(primer_valor, segundo_valor):
+    aux = primer_valor
+    primer_valor = segundo_valor
+    segundo_valor = aux
+    return primer_valor, segundo_valor
 
 
 def barajar(lista_de_naipes):
     for i in range(len(lista_de_naipes)):
         nueva_pos = random.randrange(len(lista_de_naipes))
+
+        '''
+        Intercambio de cartas, tecnica vaso vacío
+        '''
         aux = lista_de_naipes[nueva_pos]
         lista_de_naipes[nueva_pos] = lista_de_naipes[i]
         lista_de_naipes[i] = aux
     return lista_de_naipes
-
-print (barajar(crearBaraja()))
---
+++ main.py
@@ -0,0 +1,15 @@
+import barajaC
+
+palos = ['o', 'c', 'e', 'b']
+numeros = ['A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R']
+
+miBaraja = barajaC.Baraja(palos, numeros)
+
+miBaraja.mazacote[2]
+
+print(miBaraja.mazacote)
+
+print(miBaraja.repartir(3, 5))
+
+miBaraja.barajar()
+print(miBaraja.mazacote)
